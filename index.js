bea2d18fcc92f42a5139d715da433b8266afc4e5
sharath34 <sharathslash34@gmail.com>
2016-03-28 03:10:45 -0700

    loadeffect

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ var port=process.env.PORT || 8080
 
 app.use(express.static(__dirname+'/'))
 
+app.use(function(req, res, next) {
+ res.setHeader('Access-Control-Allow-Origin', '*');
+ res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+ res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
+ next();
+});
+
 app.get('*',function(req,res) {
 	
 	res.sendFile(path.join(__dirname+'/home.html'));
